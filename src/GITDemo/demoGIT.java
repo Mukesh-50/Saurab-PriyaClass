0595e21d46e7588da36066458f6143752c50c855
Priya Vashisth Tripathi <priya.vashsisth@gmail.com>
2014-11-19 23:43:03 -0800

    Update demoGIT.java

--- src/GITDemo/demoGIT.java
+++ src/GITDemo/demoGIT.java
@@ -2,10 +2,21 @@ package GITDemo;
 
 public class demoGIT {
 
-	
+	@Test
 	public void test1(){
 		
 		System.out.println("Hello GIT");
 		System.out.println("Hello Selenium ")
 	}
+	@BeforeTest
+	public void Asserttest2(){
+		FirefoxDriver driver=new FirefoxDriver();
+		driver.get("https://www.facebook.com");
+		String data_actual= driver.findElement(By.xpath(".//*[@id='login_form']/table/tbody/tr[3]/td[2]/a")).getText();
+		String data_execpted="Click to reset password";
+		Assert.assertEquals(data_actual,data_execpted);
+		driver.close();
+		
+	}
+	
 }
